da29086e1033d94ec422afa5e957ebdfaf6cd8e4
toponeclub <alishan.composer@gmail.com>
2025-11-29 10:48:11 +0100

    Update main.jsx

--- imchat-pro/src/main.jsx
+++ imchat-pro/src/main.jsx
@@ -1,12 +1,31 @@
+// src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-// ЭТА СТРОКА ДОЛЖНА БЫТЬ ПЕРВОЙ! 
-import './index.css' 
+// ИМПОРТЫ WAGMI И WEBSMODAL
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { config, projectId, chains } from './wagmi.config.js'
+
+// Инициализация Web3Modal
+createWeb3Modal({ 
+  wagmiConfig: config, 
+  projectId, 
+  chains 
+})
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}> 
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 )
